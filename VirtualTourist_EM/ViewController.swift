0ae238e1858494ef6aea084a7954a045124e6b38
EitanMagen1 <eitan.magen1@gmail.com>
2016-01-24 13:47:10 +0200

    Initial Commit

---
+++ VirtualTourist_EM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VirtualTourist_EM
+//
+//  Created by Lauren Efron on 24/01/2016.
+//  Copyright © 2016 Eitan_Magen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
